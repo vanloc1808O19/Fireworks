690efb5ff0e502bbb6c9d7bf5daa11f3dd205dd6
vanloc1808 <vanloc1808@gmail.com>
2022-01-30 14:39:16 +0700

    implement Fireworks

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 CC = g++
 
-all: Particle.o main.o
-	$(CC) Particle.o main.o -o fireworks -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
+all: Particle.o Fireworks.o main.o
+	$(CC) Particle.o Fireworks.o main.o -o fireworks -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 
 Particle.o: src/Particle.cpp header/Particle.hpp
 	$(CC) -g -c src/Particle.cpp
 
-main.o: header/Particle.hpp src/main.cpp
+Fireworks.o: src/Fireworks.cpp header/Fireworks.hpp
+	$(CC) -g -c src/Fireworks.cpp
+
+main.o: header/Particle.hpp header/Fireworks.hpp src/main.cpp
 	$(CC) -g -c src/main.cpp
 
 clean:
---
+++ src/Fireworks.cpp
@@ -0,0 +1,51 @@
+#include "../header/Fireworks.hpp"
+
+Firework::Firework(std::vector<Particle*>* p) {
+    this->_color = sf::Color(rand() % 256, rand() % 256, rand() % 256);
+
+    this->_position = sf::Vector2f(rand() % 801, 600);
+
+    this->_velocity = sf::Vector2f(rand() % 5 + 1, (rand() % 8 + 3)* (-1));
+
+    if (this->_position.x >= 400) {
+        this->_velocity.x *= -1;
+    }
+
+    this->_tick = 0;
+    
+    this->_particles = p;
+}
+
+Firework::~Firework() {
+    for (int i = 0; i < this->_particles->size(); i++) {
+        delete this->_particles->at(i);
+
+        this->_particles->at(i) = nullptr;
+    }
+}
+
+sf::Color Firework::getColor() {
+    return this->_color;
+}
+
+sf::Vector2f Firework::getPosition() {
+    return this->_position;
+}
+
+bool Firework::poll() {
+    this->_position += this->_velocity;
+
+    for (int i = 0; i < this->_velocity.y * (-1); i++) {
+        for (int j = 0; j < 5; j++) {
+            this->_particles->push_back(new Particle(this->getColor(), sf::Vector2f(this->getPosition().x,this->getPosition().y + i)));
+        }
+    }
+
+    if (this->_tick >= 50) {
+        return false;
+    }
+
+    this->_tick++;
+
+    return true;
+}
